c7c8be5e9dc071640e53b45c1a31b05e641fbaa4
chrisq32 <cwq23drexel@gmail.com>
2020-05-27 16:24:07 -0400

    Added scrapeImages()

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const cors = require('cors')({ origin: true });
 const cheerio = require('cheerio');
 const getUrls = require('get-urls');
 const fetch = require('node-fetch');
+const puppeteer = require('puppeteer');
 
+
+// Scrape meta tags from a body of text
 const scrapeMetaTags = (text) => {
 
     const urls = Array.from(getUrls(text));
 
     const requests = urls.map(async url => {
         const res = await fetch(url);
-
         const html = await res.text();
         const $ = cheerio.load(html);
 
@@ -36,12 +38,50 @@ const scrapeMetaTags = (text) => {
 
 }
 
+// Scrape images from instagram given username
+const scrapeImages = async (username) => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    await page.goto('https://www.instagram.com/accounts/login/');
+
+
+    // Login form
+    await page.screenshot({ path: '1.png' });
+    await page.type('[name=username]', 'fireship_dev');
+    await page.type('[name=password]', 'some-pa$$word');
+    await page.screenshot({ path: '2.png' });
+    await page.click('[type=submit]');
+
+    // Social Page
+    await page.waitFor(5000);
+    await page.goto(`https://www.instagram.com/${username}`);
+    await page.waitForSelector('img ', {
+        visible: true,
+    });
+
+
+    await page.screenshot({ path: '3.png' });
+
+
+    // Execute code in the DOM
+    const data = await page.evaluate(() => {
+        const images = document.querySelectorAll('img');
+        const urls = Array.from(images).map(v => v.src);
+        return urls
+    });
+
+    await browser.close();
+    console.log(data);
+    return data;
+}
+
 
 
 exports.scraper = functions.https.onRequest((request, response) => {
     cors(request, response, async () => {
         const body = request.body;
-        const data = await scrapeMetaTags(body.text);
+        const data = await scrapeImages(body.text);
 
         response.send(data);
     });
